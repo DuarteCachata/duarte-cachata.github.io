9830327a23a14038e2f715e7fca9563113aedb08
DuarteCachata <160623580+DuarteCachata@users.noreply.github.com>
2024-11-13 10:46:37 +0000

    lab8

---
+++ lab8/main.js
@@ -0,0 +1,62 @@
+const over = document.querySelector('#over');
+const btnred = document.querySelector('#btnRed')
+const btngreen = document.querySelector('#btnGreen')
+const btnblue = document.querySelector('#btnBlue')
+const TextoCor = document.querySelector('#texto-cor')
+const background = document.querySelector('#background')
+const btnconta = document.querySelector('#btnConta')
+const stringcount = document.querySelector('#counter')
+const backcolor = document.querySelector('#backColor')
+const body = document.querySelector('body')
+let random = 0;
+let counter = 0;
+
+function mudarTexto() {
+    if (over.innerText == "Passa por aqui!"){
+        over.innerText = "Obrigado por passares!";
+    }else{
+        over.innerText = "Passa por aqui!";
+    }
+}
+
+function button_red(){
+    TextoCor.style.color = "red";
+}
+
+function button_green(){
+    TextoCor.style.color = "green";
+}
+
+function button_blue(){
+    TextoCor.style.color = "blue";
+}
+
+function muda_aleatorio(){
+    random++;
+    if(random % 4 == 0) background.style.backgroundColor = 'grey'
+    if(random % 4 == 1) background.style.backgroundColor = 'lightblue'
+    if(random % 4 == 2) background.style.backgroundColor = 'salmon'
+    if(random % 4 == 3) background.style.backgroundColor = 'khaki'
+
+}
+
+function count(){
+    counter++;
+    stringcount.innerHTML=counter;
+}
+
+function changeBackgroundColor() {  
+    const colorinput = document.querySelector('#colorInput').value
+    body.style.backgroundColor = colorinput;
+}
+
+
+over.addEventListener('mouseover', mudarTexto);
+over.addEventListener('mouseout', mudarTexto);
+btnred.addEventListener('click',button_red);
+btngreen.addEventListener('click',button_green);
+btnblue.addEventListener('click',button_blue);
+background.addEventListener('keyup', muda_aleatorio);
+btnconta.addEventListener('click',count);
+backcolor.addEventListener('click',changeBackgroundColor);
+
